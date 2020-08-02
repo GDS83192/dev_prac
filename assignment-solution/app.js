f31cbe56e7d14e85b51ac9799c7889d99f89babd
graha <graham.squires3@gmail.com>
2020-08-02 19:35:20 -0400

    fix5

--- assignment-solution/app.js
+++ assignment-solution/app.js
@@ -68,8 +68,9 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
+    app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
   });
